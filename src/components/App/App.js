94425bc0b03c4153759806a08db3dbd64882b0cc
Dom <dvlasic.dev@gmail.com>
2019-04-25 19:55:01 +0200

    added basic styling and some calculator operations

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  display: flex;
+  background: linear-gradient(
+    180deg,
+    rgba(37, 36, 55, 1) 0%,
+    rgba(49, 48, 66, 1) 31%,
+    rgba(58, 58, 75, 1) 58%,
+    rgba(67, 74, 83, 1) 95%
+  );
+`;
+const Container = styled.section`
+  display: flex;
+  max-width: 600px;
+  width: 100%;
+
+  margin: 100px auto;
+  border: 3px solid grey;
+  padding: 10px;
+`;
+const Display = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: white;
+  width: 100%;
+  justify-content: flex-end;
+  p {
+    align-self: flex-end;
+  }
+`;
 function App() {
+  const [input, setInput] = useState(0);
+  const [display, setDisplay] = useState(0);
+  const [operation, setOperation] = useState(null);
+
+  const handleKeyDown = e => {
+    let { key } = e;
+
+    if (/^[0-9]*$/.test(key)) {
+      e.preventDefault();
+      setInput(String(input + parseInt(key, 10)));
+    }
+    if (key === 'Backspace') {
+      setInput(
+        input.length === 1 ? 0 : String(input.slice(0, input.length - 1))
+      );
+    }
+    if (key == 'c') {
+      setDisplay(0);
+      setInput(0);
+      setOperation(null);
+    }
+    if (key === '+') {
+      setOperation(' + ');
+      setDisplay(Number(display) + Number(input));
+      setInput(0);
+    }
+    if (key === '*') {
+      setOperation(' * ');
+      if (input === 0 && display !== 0) {
+        setDisplay(display * display);
+      } else {
+        setDisplay(
+          display === 0 ? Number(input) : Number(display) * Number(input)
+        );
+      }
+      setInput(0);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Container>
+        <Display>
+          <p>
+            {display}
+            {operation}
+          </p>
+          <p>{input}</p>
+        </Display>
+      </Container>
+    </Wrapper>
   );
 }
 
