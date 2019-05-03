99401638bafde8577099c69aeb2588193e22d512
Dom <dvlasic.dev@gmail.com>
2019-05-03 16:19:23 +0200

    added dot, number sign change, one func for input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import Backspace from './Backspace';
 import styled from 'styled-components';
+import { buttons } from '../../constants/';
 
 const Wrapper = styled.div`
   * {
@@ -18,12 +20,11 @@ const Wrapper = styled.div`
 `;
 const Container = styled.section`
   display: flex;
-  max-width: 345px;
+  max-width: 325px;
   width: 100%;
   flex-direction: column;
   margin: 100px auto;
-  border: 3px solid grey;
-  padding: 10px;
+  border: 3px solid #57606f;
 `;
 const Display = styled.div`
   display: flex;
@@ -31,7 +32,9 @@ const Display = styled.div`
   color: white;
   width: 100%;
   justify-content: flex-end;
-  margin-bottom: 20px;
+  margin-bottom: 50px;
+  padding: 10px 10px 20px 10px;
+  overflow: hidden;
   p {
     align-self: flex-end;
   }
@@ -45,6 +48,9 @@ const Display = styled.div`
 const ButtonsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  max-width: 300px;
+  align-self: center;
+  margin-bottom: 5px;
   div:nth-child(17) {
     flex-basis: 50%;
     max-width: 200px;
@@ -52,23 +58,46 @@ const ButtonsContainer = styled.div`
 `;
 const Item = styled.div`
   display: flex;
-  max-width: 80px;
   flex-basis: 25%;
+  max-width: 75px;
   width: 100%;
   height: 70px;
 
   button {
+    background: 0;
+    border: 0;
+    outline: 0;
     width: 100%;
     font-size: 25px;
+    position: relative;
+    background: radial-gradient(
+      circle,
+      rgba(176, 176, 176, 1) 0%,
+      rgba(218, 218, 218, 1) 100%
+    );
+
+    border: 2px solid #353b48;
+    &:active {
+      top: 1px;
+    }
+    &:hover {
+      opacity: 0.9;
+    }
+    svg {
+      display: flex;
+      margin: auto;
+    }
   }
 `;
+
 function App() {
   const [input, setInput] = useState(0);
   const [display, setDisplay] = useState(0);
   const [operation, setOperation] = useState(null);
 
-  const handleKeyDown = e => {
+  const handleKeyDownAndClick = (e, item) => {
     let { key } = e;
+    const value = item ? item : key;
     const operators = {
       '+': (input, display) => Number(display) + Number(input),
       '*': (input, display) => display * input,
@@ -76,85 +105,86 @@ function App() {
       '/': (input, display) => display / input
     };
 
-    if (/^[0-9]*$/.test(key)) {
+    if (/^[0-9]*$/.test(value)) {
       e.preventDefault();
-      setInput(String(input + parseInt(key, 10)));
+      setInput(String(input + parseInt(value, 10)));
     }
-    if (key === 'Backspace') {
+    if (value === 'Backspace' || value === '<=') {
+      e.preventDefault();
       setInput(
         input.length === 1 || input === 0
           ? 0
           : String(input.slice(0, input.length - 1))
       );
     }
-    if (key === 'c') {
+    if (value === 'c' || value === 'C') {
       setDisplay(0);
       setInput(0);
       setOperation(null);
     }
-    if (key === 'Enter') {
+    if (value === 'Enter' || value === '=') {
+      e.preventDefault();
       if (input !== 0 && operation !== null) {
         setInput(0);
         setDisplay(operators[operation.trim()](input, display));
       }
     }
+    if (value === '.') {
+      if (!/\./.test(input)) {
+        setInput(input + '.');
+      }
+    }
+    if (value === '+/-') {
+      setDisplay('-1' * display);
+    }
 
-    if (key in operators) {
-      setOperation(` ${key} `);
+    if (value in operators) {
+      setOperation(` ${value} `);
       setInput(0);
-      if (input === 0 && ` ${key} ` === operation) {
-        setDisplay(operators[key](display, display));
+      if (input === 0 && ` ${value} ` === operation) {
+        setDisplay(operators[value](display, display));
       } else if (input !== 0) {
         setDisplay(
-          display === 0 ? Number(input) : operators[key](input, display)
+          display === 0 ? Number(input) : operators[value](input, display)
         );
       }
     }
   };
+
   useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keydown', handleKeyDownAndClick);
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keydown', handleKeyDownAndClick);
     };
   });
-  const buttons = [
-    'C',
-    '<=',
-    '+-',
-    '/',
-    '7',
-    '8',
-    '9',
-    'X',
-    '4',
-    '5',
-    '6',
-    '-',
-    '1',
-    '2',
-    '3',
-    '+',
-    '0',
-    '.',
-    '='
-  ];
+  const language = navigator.language || 'en-US';
   return (
     <Wrapper>
       <Container>
         <Display>
           <p>
-            {display.toString().replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, ' ')}
+            {/* display.toString().replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, ' ') */}
+            {parseFloat(display).toLocaleString(language, {
+              useGrouping: true,
+              maximumFractionDigits: 6
+            })}
             {operation}
           </p>
           <p>
-            {input.toString().replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, ' ')}
+            {/* input.toString().replace(/(?!^)(?=(?:\d{3})+(?:\.|$))/gm, ' ') */}
+            {parseFloat(input).toLocaleString(language, {
+              useGrouping: true,
+              maximumFractionDigits: 6
+            })}
           </p>
         </Display>
         <ButtonsContainer>
           {buttons.map(item => {
             return (
               <Item key={item}>
-                <button>{item}</button>
+                <button onClick={e => handleKeyDownAndClick(e, item)}>
+                  {item === '<=' ? <Backspace /> : item === '*' ? 'X' : item}
+                </button>
               </Item>
             );
           })}
---
+++ src/components/App/Backspace.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Backspace = () => {
+  return (
+    <svg
+      width="40"
+      height="30"
+      viewBox="0 0 52 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M48.5 1H15.8072C15.2894 1 14.7917 1.20086 14.4189 1.56031L2.44061 13.1108C1.64513 13.8779 1.6225 15.1447 2.39008 15.9397L14.3987 28.3772C14.7824 28.7746 15.3133 28.9956 15.8656 28.9878L48.5282 28.5278C49.6216 28.5124 50.5 27.6216 50.5 26.528V14.5V3C50.5 1.89543 49.6046 1 48.5 1Z"
+        stroke="black"
+        strokeWidth="2"
+      />
+      <path
+        d="M24.5 9L36 20.5M36 9L24.5 21"
+        stroke="black"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
+
+export default Backspace;
